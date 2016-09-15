51d8dbb13ec4046e69933832ab0887e55f916070
Porcelani <porcelani@gmail.com>
2016-09-15 08:00:05 -0300

    new install comands

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -14,3 +14,20 @@ apt-get install terminator
 add-apt-repository ppa:webupd8team/atom -y
 apt-get update
 apt-get install atom
+
+#CopyQ
+add-apt-repository ppa:noobslab/indicators -y
+apt-get update
+apt-get install copyq
+
+#Paint
+apt-get install kolourpaint4 -y
+
+#PrintScreen
+apt-get install shutter -y
+
+#Foxit
+# wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.1/en_us/FoxitReader2.1.0805_Server_x64_enu_Setup.run.tar.gz
+# tar xzvf FoxitReader*.tar.gz
+# chmod a+x FoxitReader*.run
+# ./FoxitReader.*.run
