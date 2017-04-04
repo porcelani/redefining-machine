05350ecac799b3746bf692c6768b3f7835417a5e
porcelani <porcelani@gmail.com>
2017-04-04 18:30:36 -0300

    install linux

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo su -
 
 #Java
@@ -6,15 +7,16 @@ apt-get update
 apt-get install oracle-java8-installer
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
+#Maven
+apt-get install maven -y
+
 #Git
 apt-get install git
 git config --global user.email "porcelani@gmail.com"
 git config --global user.name "Porcelani"
 
-#Terminator
-add-apt-repository ppa:gnome-terminator -y
-apt-get update
-apt-get install terminator
+#Guake(Terminal)
+apt-get install guake -y
 
 #Atom
 add-apt-repository ppa:webupd8team/atom -y
@@ -36,3 +38,16 @@ apt-get install shutter -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 apt-get install -y nodejs
 apt-get install -y build-essential
+
+#Docker
+#https://gist.github.com/wdullaer/f1af16bd7e970389bad3
+wget -qO- https://get.docker.com/ | sh
+
+#Docker-compose
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
