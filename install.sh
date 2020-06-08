0c370d122aa2e60d1ce112ee1f19cdb9da661c54
c <porcelani@gmail.com>
2020-06-08 16:06:04 -0300

    update

--- install.sh
+++ install.sh
@@ -1,94 +1,143 @@
 #!/usr/bin/env bash
 
-#https://askubuntu.com/questions/79280/how-to-install-chrome-browser-properly-via-command-line
-#Chrome
-sudo apt-get install libxss1 libappindicator1 libindicator7 -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
-sudo apt-get install -f
+sudo apt-get update
 
-#Visual Code
+echo 'installing tools' 
+sudo apt install curl -y
+sudo apt install jq -y
+sudo apt install vim -y
+sudo apt-get install xclip -y
+sudo apt-get install awscli -y
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+
+echo 'installing git' 
+sudo apt install git -y
+git config --global user.email "porcelani@gmail.com"
+git config --global user.name "Porcelani"
+git config --global core.editor vim
+
+echo "Generating a SSH Key"
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+
+echo 'installing zsh'
+#https://medium.com/@ivanaugustobd/seu-terminal-pode-ser-muito-muito-mais-produtivo-3159c8ef77b2
+sudo apt-get install zsh -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
+
+echo 'installing plugins' 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting 
+echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+echo 'installing asdf'
+#https://asdf-vm.com/#/core-manage-asdf-vm
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+echo ". $HOME/.asdf/asdf.sh" >> ~/.zshrc # or add asdf as a pluging EX: plugins=(git asdf)
+source ~/.zshrc
+
+#https://dancroak.com/asdf-version-manager
+asdf_plugin_update() {
+  if ! asdf plugin-list | grep -Fq "$1"; then
+    asdf plugin-add "$1" "$2"
+  fi
+  asdf plugin-update "$1"
+}
+
+asdf_plugin_update "nodejs" "https://github.com/asdf-vm/asdf-nodejs"
+export NODEJS_CHECK_SIGNATURES=no
+asdf install nodejs 13.7.0
+echo "nodejs 13.7.0" >> ~/.tool-versions
+
+asdf_plugin_update "ruby" "https://github.com/asdf-vm/asdf-ruby"
+asdf install ruby 2.5.1
+echo "ruby 2.5.1" >> ~/.tool-versions
+
+asdf_plugin_update "java" "https://github.com/halcyon/asdf-java.git"
+asdf install java adopt-openjdk-11.0.7+10
+echo "java adopt-openjdk-11.0.7+10" >> ~/.tool-versions
+
+asdf_plugin_update "sbt"
+asdf install sbt 1.1.1
+echo "sbt 1.1.1" >> ~/.tool-versions
+
+asdf_plugin_update "scala" "https://github.com/mtatheonly/asdf-scala"
+asdf install scala 2.12.10
+echo "scala 2.12.10" >> ~/.tool-versions
+
+echo 'installing fzf'
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+echo 'installing terminator'
+sudo apt-get install terminator -y
+
+echo 'installing Guake'
+#Config: https://askubuntu.com/questions/136559/guake-doesnt-start-when-session-starts
+sudo apt-get install guake -y
+
+
+echo 'installing code'
 #https://code.visualstudio.com/docs/setup/linux
 #https://code.visualstudio.com/docs/getstarted/settings
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update -y
-sudo apt-get install code -y
-
-# Intellij
-# http://ubuntuhandbook.org/index.php/2017/07/intellij-idea-2017-2-released-how-to-install-in-ubuntu/
-sudo add-apt-repository ppa:ubuntuhandbook1/apps
+sudo apt-get install apt-transport-https -y
 sudo apt-get update
-sudo apt-get install intellij-idea-community
+sudo apt-get install code -y # or code-insiderssudo apt-get install xclip -y
 
-# Git
-apt-get install git
-git config --global user.email "porcelani@gmail.com"
-git config --global user.name "Porcelani"
-git config --global http.sslverify false
+echo 'installing extensions'
+code --install-extension Shan.code-settings-sync
 
-#SVN
-sudo apt install subversion -y
 
-#Docker
-#https://gist.github.com/wdullaer/f1af16bd7e970389bad3
-wget -qO- https://get.docker.com/ | sh
-##Docker-compose
-COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+echo 'installing docker' 
+sudo apt-get remove docker docker-engine docker.io
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+chmod 777 /var/run/docker.sock
+sudo usermod -aG docker $USER
+sudo groupadd docker
+newgrp docker
+docker run hello-world
+
+echo 'installing docker-compose' 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
-sudo gpasswd -a ${USER} docker
-sudo service docker restart
-
-#Java Ecosystem
-#https://sdkman.io/install
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-#Node
-#https://github.com/creationix/nvm
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install --lts
+docker-compose --version
 
 
-#CopyQ
+echo 'installing CopyQ'
 #Config: Preferences > Shoutcuts > Show/Hide main Window > Global Shoutcut 
 sudo add-apt-repository ppa:noobslab/indicators -y
 sudo apt-get update
 sudo apt-get install copyq -y
 
-#PrintScreen - Shutter
+echo 'installing Shutter'
 # http://askubuntu.com/questions/456985/how-to-change-the-keyboard-shortcut-to-take-screenshots-with-shutter
+sudo add-apt-repository -y ppa:linuxuprising/shutter
+sudo apt-get update
 sudo apt-get install shutter -y
 
-#Guake(Terminal)
-#Config: https://askubuntu.com/questions/136559/guake-doesnt-start-when-session-starts
-sudo apt-get install guake -y
-
-#DBeaver
+echo 'installing DBeaver'
 # https://dbeaver.io/download/
 sudo add-apt-repository ppa:serge-rider/dbeaver-ce
 sudo apt-get update
 sudo apt-get install dbeaver-ce
 
+echo 'installing chrome' 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-#Sexy-bash
-(cd /tmp && git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
-
-#suporte 32bits
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 libxrender1:i386 libxtst6:i386 libxi6:i386
-
-#Kitematic
-docker run --net host --name kitematic \
--v /tmp/.X11-unix:/tmp/.X11-unix \
--e DISPLAY=$DISPLAY \
--v $HOME/.Xauthority:/root/.Xauthority \
--v /tmp/.X11-unix:/tmp/.X11-unix \
--v /var/run/docker.sock:/var/run/docker.sock \
---privileged=true -t jonadev95/kitematic-docker
+echo 'installing slack' 
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
+sudo apt install ./slack-desktop-*.deb -y
